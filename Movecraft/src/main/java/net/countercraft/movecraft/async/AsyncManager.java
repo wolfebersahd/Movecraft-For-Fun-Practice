945d906657d0b3c5001723a9a92a500f69dbbc6f
wolfebersahd <j_wolfeinc@hotmail.com>
2025-02-22 10:49:23 -0500

    Update AsyncManager.java

--- Movecraft/src/main/java/net/countercraft/movecraft/async/AsyncManager.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/async/AsyncManager.java
@@ -31,6 +31,8 @@ import net.countercraft.movecraft.craft.SinkingCraft;
 import net.countercraft.movecraft.craft.type.CraftType;
 import net.countercraft.movecraft.events.CraftReleaseEvent;
 import net.countercraft.movecraft.mapUpdater.MapUpdateManager;
+import net.countercraft.movecraft.util.hitboxes.HitBox;
+import net.countercraft.movecraft.util.hitboxes.SolidHitBox;
 import net.kyori.adventure.text.Component;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
